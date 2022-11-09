73ff84847f4217eff33d040550e9b3b2172e400f
Jonathan123452627727 <117828516+Jonathan123452627727@users.noreply.github.com>
2022-11-09 09:53:46 -0300

    Add files via upload
    iniciando

---
+++ document.py
@@ -0,0 +1,23 @@
+#IF
+fecha=int(input("ingrese año"))
+edad=2022-fecha
+if not edad>18:
+	print("La persona es mayor de edad")
+else:
+	print("La persona es menor de edad")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
